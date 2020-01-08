10511e71dffa9df119c665e0bc3f7e46dfd16020
Ausan Singh <ausan.singh@vfirst.com>
2020-01-08 14:54:56 +0530

    profile update

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  totalExp = '';
+  ageToday = '';
+  constructor() { }
+
+  ngOnInit() {
+    let today = new Date();
+    let expDiff = this.dateDiff('2011-09-15', today);
+    this.totalExp= `${expDiff.years}.${expDiff.months}`;
+    
+    let dobDiff = this.dateDiff('1991-07-10', today);
+    this.ageToday= `${dobDiff.years}`;
+  }
+
+  dateDiff(startingDate, endingDate) {
+    var startDate = new Date(new Date(startingDate).toISOString().substr(0, 10));
+    if (!endingDate) {
+      endingDate = new Date().toISOString().substr(0, 10);    // need date in YYYY-MM-DD format
+    }
+    var endDate = new Date(endingDate);
+    if (startDate > endDate) {
+      var swap = startDate;
+      startDate = endDate;
+      endDate = swap;
+    }
+    var startYear = startDate.getFullYear();
+    var february = (startYear % 4 === 0 && startYear % 100 !== 0) || startYear % 400 === 0 ? 29 : 28;
+    var daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    var yearDiff = endDate.getFullYear() - startYear;
+    var monthDiff = endDate.getMonth() - startDate.getMonth();
+    if (monthDiff < 0) {
+      yearDiff--;
+      monthDiff += 12;
+    }
+    var dayDiff = endDate.getDate() - startDate.getDate();
+    if (dayDiff < 0) {
+      if (monthDiff > 0) {
+        monthDiff--;
+      } else {
+        yearDiff--;
+        monthDiff = 11;
+      }
+      dayDiff += daysInMonth[startDate.getMonth()];
+    }
+
+    return {'years':yearDiff, 'months':monthDiff,'days':dayDiff};
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    EducationComponent,
+    ExperienceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
